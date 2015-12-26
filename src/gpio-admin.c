55231f8b7fdd1f64d92b312b06fe5ea023053248
Vincent Trumpff <vincent@stoic.com>
2015-12-26 18:40:29 +0100

    gpio access path

--- src/gpio-admin.c
+++ src/gpio-admin.c
@@ -28,7 +28,7 @@ static void usage_error(char **argv) {
 static void allow_access_by_user(unsigned int pin, const char *filename) {
   char path[PATH_MAX];
   struct stat stat_data;
-  int size = snprintf(path, PATH_MAX, "/sys/devices/virtual/gpio/gpio%u/%s", pin, filename);
+  int size = snprintf(path, PATH_MAX, GPIO_CLASS_PATH "gpio%u/%s", pin, filename);
 
   if (size >= PATH_MAX) {
     error(7, 0, "path of GPIO pin is too long!");
